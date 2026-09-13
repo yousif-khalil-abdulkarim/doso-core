c67ace3a372dcbf169c7cec2736ec5976561d101
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-13 19:09:33 +0200

    feature(middleware): Export hook middlewares from barrels
    Wire the hooks barrel into the internal and public middleware module barrels.

--- src/middleware/implementations/_module.ts
+++ src/middleware/implementations/_module.ts
@@ -1,3 +1,4 @@
 export * from "@/middleware/implementations/enhance-factory/_module.js";
 export * from "@/middleware/implementations/use-factory/_module.js";
 export * from "@/middleware/implementations/with-plugin-factory/_module.js";
+export * from "@/middleware/implementations/hooks/_module.js";
