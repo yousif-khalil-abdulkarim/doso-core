b057d7d454ddd59b366e6364e7b3635b23ca6f1c
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-14 18:35:47 +0200

    enhancement(transaction-context): Export resolveTransactionAware from derivables barrel
    Exposes resolveTransactionAware so database adapters can resolve a TransactionAware client into an ITransactionContext through the transaction-context derivables barrel.

--- src/transaction-context/implementations/derivables/transaction-context/_module.ts
+++ src/transaction-context/implementations/derivables/transaction-context/_module.ts
@@ -1,1 +1,2 @@
 export * from "@/transaction-context/implementations/derivables/transaction-context/transaction-context.js";
+export * from "@/transaction-context/implementations/derivables/transaction-context/resolve-transaction-aware.js";
